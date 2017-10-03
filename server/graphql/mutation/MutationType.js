a60ff2b532c2485453f34b5e14e60aefb1ba88ca
Johannes Kettmann <jkettmann@gmx.net>
2017-10-03 10:45:38 +0200

    Removed login and logout mutation

--- server/graphql/mutation/MutationType.js
+++ server/graphql/mutation/MutationType.js
@@ -1,16 +1,12 @@
 import { GraphQLObjectType } from 'graphql'
 
 import RegisterMutation from '../mutation/RegisterMutation'
-import LoginMutation from '../mutation/LoginMutation'
-import LogoutMutation from '../mutation/LogoutMutation'
 import CreatePostMutation from '../mutation/CreatePostMutation'
 
 export default new GraphQLObjectType({
   name: 'Mutation',
   fields: () => ({
     register: RegisterMutation,
-    login: LoginMutation,
-    logout: LogoutMutation,
     createPost: CreatePostMutation,
   }),
 })
