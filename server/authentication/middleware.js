127db9ebf5e473c05e052f73e16bbc140301d801
Johannes Kettmann <jkettmann@gmx.net>
2017-10-03 13:00:12 +0200

    Removed passport user serialization

--- server/authentication/middleware.js
+++ server/authentication/middleware.js
@@ -4,15 +4,6 @@ import bodyParser from 'body-parser'
 import sessionMiddleware from './sessionMiddleware'
 import * as localStrategy from './localStrategy'
 
-passport.serializeUser((user, done) => {
-  console.log('serialize', user)
-  done(user)
-})
-passport.deserializeUser((user, done) => {
-  console.log('deserialize', user)
-  done(user)
-})
-
 export default (app, database) => {
   app.use(sessionMiddleware())
   app.use(bodyParser())
