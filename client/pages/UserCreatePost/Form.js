478f6b2bcc216341fb73cb7cda7c8ccbf4f7ba26
Johannes Kettmann <jkettmann@gmx.net>
2017-11-14 07:07:43 +0100

    Use redux-form on create post

--- client/pages/UserCreatePost/Form.js
+++ client/pages/UserCreatePost/Form.js
@@ -1,11 +1,61 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { reduxForm } from 'redux-form'
 import styled from 'styled-components'
-import Formsy from 'formsy-react'
 
-const Form = styled(Formsy.Form)`
+import TextField from '../../components/Input/TextField'
+import ImageField from '../../components/Input/ImageField'
+import Button from '../../components/Button'
+
+const FormWrapper = styled.form`
   max-width: 600px;
   width: 90%;
   margin-left: auto;
   margin-right: auto;
 `
 
-export default Form
+const Form = ({ handleSubmit, valid }) => (
+  <FormWrapper onSubmit={handleSubmit}>
+    <TextField
+      name="title"
+      label="Title"
+      validations="isWords"
+      validationError="Please enter a title"
+      fullWidth
+      required
+    />
+
+    <TextField
+      name="description"
+      label="Description"
+      validations="isWords"
+      validationError="Please enter a description"
+      fullWidth
+      required
+    />
+
+    <ImageField
+      label="Select Image"
+      name="image"
+      style={{ marginTop: 20 }}
+      validations="isExisty"
+      validationError="Please select an image"
+      fullWidth
+    />
+
+    <Button
+      type="submit"
+      label="Save post"
+      disabled={!valid}
+      secondary
+      fullWidth
+    />
+  </FormWrapper>
+)
+
+Form.propTypes = {
+  valid: PropTypes.bool.isRequired,
+  handleSubmit: PropTypes.func.isRequired,
+}
+
+export default reduxForm({ form: 'createPost' })(Form)
--- client/pages/UserCreatePost/UserCreatePost.js
+++ client/pages/UserCreatePost/UserCreatePost.js
@@ -1,107 +1,49 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { createFragmentContainer, graphql } from 'react-relay'
-import { routerShape } from 'found/lib/PropTypes'
+import { compose, withHandlers } from 'recompose'
 
 import Wrapper from './Wrapper'
 import Form from './Form'
-import TextInput from '../../components/Input/FormsyText'
-import ImageInput from '../../components/Input/ImageInput'
-import Button from '../../components/Button'
 import CreatePostMutation from '../../mutation/CreatePostMutation'
 
-class CreatePostPage extends React.Component {
-  static propTypes = {
-    router: routerShape.isRequired,
-    relay: PropTypes.shape({
-      environment: PropTypes.any.isRequired,
-    }).isRequired,
-  }
+const UserCreatePostPage = ({ createPost }) => (
+  <Wrapper>
+    <h2>Create Post</h2>
 
-  constructor() {
-    super()
-    this.state = {
-      canSubmit: false,
-    }
-  }
+    <Form onSubmit={createPost} />
+  </Wrapper>
+)
 
-  enableButton = () => {
-    this.setState({
-      canSubmit: true,
-    })
-  }
+UserCreatePostPage.propTypes = {
+  createPost: PropTypes.func.isRequired,
+}
 
-  disableButton = () => {
-    this.setState({
-      canSubmit: false,
-    })
-  }
-
-  createPost = ({ title, description, image }) => {
-    const environment = this.props.relay.environment
+const handlers = {
+  createPost: ({ relay, router }) => ({ title, description, image }) => {
+    const environment = relay.environment
 
     CreatePostMutation.commit({
       environment,
       input: { title, description, image },
-      onCompleted: () => this.props.router.push('/user/posts'),
-      onError: errors => console.error('Creating post Failed', errors[0]),
+      onCompleted: (result, errors) => {
+        if (!errors) {
+          router.push('/user/posts')
+          return
+        }
+        console.error('create post', errors)
+      },
+      onError: error => console.error('Creating post failed', error),
     })
-  }
-
-  render() {
-    return (
-      <Wrapper>
-        <h2>Create Post</h2>
-
-        <Form
-          onValid={this.enableButton}
-          onInvalid={this.disableButton}
-          onSubmit={this.createPost}
-        >
-
-          <TextInput
-            name="title"
-            label="Title"
-            validations="isWords"
-            validationError="Please enter a title"
-            fullWidth
-            required
-          />
-
-          <TextInput
-            name="description"
-            label="Description"
-            validations="isWords"
-            validationError="Please enter a description"
-            fullWidth
-            required
-          />
-
-          <ImageInput
-            label="Select Image"
-            name="image"
-            style={{ marginTop: 20 }}
-            validations="isExisty"
-            validationError="Please select an image"
-            fullWidth
-          />
-
-          <Button
-            type="submit"
-            label="Save post"
-            style={{ marginTop: 20 }}
-            disabled={!this.state.canSubmit}
-            secondary
-            fullWidth
-          />
-        </Form>
-      </Wrapper>
-    )
-  }
+  },
 }
 
+const enhance = compose(
+  withHandlers(handlers),
+)
+
 export default createFragmentContainer(
-  CreatePostPage,
+  enhance(UserCreatePostPage),
   graphql`
     fragment UserCreatePost on Query {
       permission {
