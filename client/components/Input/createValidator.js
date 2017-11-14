385da0088f0fb5d0edab7396abbb407d6154cbdb
Johannes Kettmann <jkettmann@gmx.net>
2017-11-14 16:45:45 +0100

    Fixed validation rules

--- client/components/Input/createValidator.js
+++ client/components/Input/createValidator.js
@@ -1,7 +1,7 @@
 const RULES = {
   letters: /^[a-z]*$/i,
-  alphaNumeric: /^[a-z0-1]*$/i,
-  number: /^[0-1]*$/i,
+  alphaNumeric: /^[a-z0-9]*$/i,
+  number: /^[0-9]*$/i,
   email: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
   password: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,}$/, // at least one letter and one number
   phone: /^[+]?[0-9()-\s.]*$/im,
